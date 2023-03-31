9d2bf9180ea0e10509c66bc33afcea25ed06b5b0
Austin Hardersen <59270607+Neddslayer@users.noreply.github.com>
2023-03-31 11:00:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var txt = 'Generating Amazon gift card code...'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter(txt) {
-  document.getElementById("generating_text").innerHTML = "";
   if (i < txt.length) {
     document.getElementById("generating_text").innerHTML += txt.charAt(i);
     i++;
@@ -16,11 +15,14 @@ function generateText() {
   setTimeout(gen3, 4000);
 }
 function gen1() {
+  document.getElementById("generating_text").innerHTML = "";
   typeWriter('Generating Amazon gift card code...');
 }
 function gen2() {
+  document.getElementById("generating_text").innerHTML = "";
   typeWriter('Attempting automatic human verification...');
 }
 function gen3() {
+  document.getElementById("generating_text").innerHTML = "";
   typeWriter('Verification complete! Outputting code.');
 }
