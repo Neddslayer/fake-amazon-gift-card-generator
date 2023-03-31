34471016c0dc6092c95f692564160381666c1bf6
Austin Hardersen <59270607+Neddslayer@users.noreply.github.com>
2023-03-31 10:44:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ var i = 0;
 var txt = 'Generating Amazon gift card code...'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
-function typeWriter() {
+function typeWriter(txt) {
   document.getElementById("generating_text").innerHTML = "";
   if (i < txt.length) {
     document.getElementById("generating_text").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
+}
+function generateText() {
+  
+}
+function gen1() {
+  typeWriter('Generating Amazon gift card code...');
+}
+function gen2() {
+  typeWriter('Attempting automatic human verification...');
+}
+function gen3() {
+  typeWriter('Verification complete! Outputting code.');
 }
